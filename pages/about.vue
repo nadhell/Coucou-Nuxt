94ae0a95c52bfb6c4dd0b70b54ac3873e626e9ad
nadhell <nadhell@live.com>
2023-02-03 11:16:59 +0100

    analytics setup

--- pages/about.vue
+++ pages/about.vue
@@ -4,7 +4,7 @@
         <div class="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div class="lg:pl-20">
             <div class="max-w-xs px-2.5 lg:max-w-none">
-              <img alt="" src="~/assets/profile.png" class="object-cover aspect-square rotate-3 rounded-2xl bg-zinc-800">
+              <img alt="" src="" class="object-cover aspect-square rotate-3 rounded-2xl bg-zinc-800">
             </div>
           </div>
           <div class="lg:order-first lg:row-span-2">
